18b3352d17bfe5c5106e3026f33af6d44000fced
Natalia <onishenko2@gmail.com>
2026-01-04 13:36:53 +0100

    hw9

--- app/(private routes)/notes/action/create/page.tsx
+++ app/(private routes)/notes/action/create/page.tsx
@@ -31,7 +31,7 @@ export default function CreateNotePage() {
     <main className={css.main}>
       <div className={css.container}>
         <h1 className={css.title}>Create note</h1>
-        <NoteForm onClose={() => {}} />
+        <NoteForm/>
       </div>
     </main>
   );
